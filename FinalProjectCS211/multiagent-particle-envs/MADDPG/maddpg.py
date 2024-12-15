eeb1ca1ddfb17dd7a704c8e583b6d1a5fe996b90
huynhchinhan <nhan00554@gmail.com>
2024-12-15 20:25:42 +0700

    Using pettingzoo instead

--- FinalProjectCS211/multiagent-particle-envs/MADDPG/maddpg.py
+++ FinalProjectCS211/multiagent-particle-envs/MADDPG/maddpg.py
@@ -1,6 +1,9 @@
 import torch as T
 import torch.nn.functional as F
 from agent import Agent
+from pettingzoo.mpe import simple_adversary_v3
+env = simple_adversary_v3.env(N=2, max_cycles=25, continuous_actions=False)
+env.reset()
 
 class MADDPG:
     def __init__(self, actor_dims, critic_dims, n_agents, n_actions, 
@@ -9,21 +12,26 @@ class MADDPG:
         self.agents = []
         self.n_agents = n_agents
         self.n_actions = n_actions
-        chkpt_dir += scenario 
-        for agent_idx in range(self.n_agents):
-            self.agents.append(Agent(actor_dims[agent_idx], critic_dims,  
-                            n_actions, n_agents, agent_idx, alpha=alpha, beta=beta,
-                            chkpt_dir=chkpt_dir))
+        chkpt_dir += scenario
+        self.agents = {}
+        for agent_idx, agent_name in enumerate(env.possible_agents):
+            self.agents[agent_name] = Agent(actor_dims[agent_idx],
+                                            critic_dims,
+                                            n_actions, n_agents,
+                                            agent_name = agent_name,
+                                            alpha=alpha,
+                                            beta=beta,
+                                            chkpt_dir=chkpt_dir)
 
 
     def save_checkpoint(self):
         print('... saving checkpoint ...')
-        for agent in self.agents:
+        for agent_name, agent in self.agents.items():
             agent.save_models()
 
     def load_checkpoint(self):
         print('... loading checkpoint ...')
-        for agent in self.agents:
+        for agent_name, agent in self.agents.items():
             agent.load_models()
 
     def choose_action(self, raw_obs):
@@ -33,6 +41,10 @@ class MADDPG:
             actions.append(action)
         return actions
 
+    def choose_action(self, raw_obs):
+        actions = {agent.agent_name: agent.choose_action(raw_obs[agent.agent_name]) for agent in self.agents.values()}
+        return actions
+
     def learn(self, memory):
         if not memory.ready():
             return
@@ -40,61 +52,63 @@ class MADDPG:
         actor_states, states, actions, rewards, \
         actor_new_states, states_, dones = memory.sample_buffer()
 
-        device = self.agents[0].actor.device
+        device = self.agents['adversary_0'].actor.device
 
         states = T.tensor(states, dtype=T.float).to(device)
         actions = T.tensor(actions, dtype=T.float).to(device)
-        rewards = T.tensor(rewards, dtype=T.float).to(device)
+        rewards = T.tensor(rewards).to(device)
         states_ = T.tensor(states_, dtype=T.float).to(device)
         dones = T.tensor(dones).to(device)
 
+        all_agents_new_actions = []
+        all_agents_new_mu_actions = []
         old_agents_actions = []
 
-        for agent_idx, agent in enumerate(self.agents):
+        for agent_idx,(agent_name, agent) in enumerate(self.agents.items()):
+            new_states = T.tensor(actor_new_states[agent_idx], 
+                                 dtype=T.float).to(device)
+            new_pi = agent.target_actor.forward(new_states)
+
+            all_agents_new_actions.append(new_pi)
+
+
+            mu_states = T.tensor(actor_states[agent_idx], 
+                                 dtype=T.float).to(device)
+            pi = agent.actor.forward(mu_states)
+
+            all_agents_new_mu_actions.append(pi)
+
             old_agents_actions.append(actions[agent_idx])
 
+
+
+
+        new_actions = T.cat([acts for acts in all_agents_new_actions], dim=1)
+        mu = T.cat([acts for acts in all_agents_new_mu_actions], dim=1)
         old_actions = T.cat([acts for acts in old_agents_actions],dim=1)
 
-        new_actions = []
-        mu = []
-        for i in range(self.n_agents):
-            all_agents_new_actions = []
-            all_agents_new_mu_actions = []
-            for agent_idx, agent in enumerate(self.agents):
-                new_states = T.tensor(actor_new_states[agent_idx], 
-                                    dtype=T.float).to(device)
-
-                new_pi = agent.target_actor.forward(new_states)
 
 
-                mu_states = T.tensor(actor_states[agent_idx], 
-                                    dtype=T.float).to(device)
-                pi = agent.actor.forward(mu_states)
-                if i == agent_idx:
-                    all_agents_new_actions.append(new_pi)
-                    all_agents_new_mu_actions.append(pi)
-                else:
-                    all_agents_new_actions.append(new_pi.detach())
-                    all_agents_new_mu_actions.append(pi.detach())
 
-            new_actions.append(T.cat([acts for acts in all_agents_new_actions], dim=1))
-            mu.append(T.cat([acts for acts in all_agents_new_mu_actions], dim=1))
+        for agent_idx,(agent_name, agent) in enumerate(self.agents.items()):
 
-        for agent_idx, agent in enumerate(self.agents):
-            critic_value_ = agent.target_critic.forward(states_, new_actions[agent_idx]).flatten()
+            critic_value_ = agent.target_critic.forward(states_, new_actions).flatten()
+
             critic_value_[dones[:,0]] = 0.0
-            critic_value = agent.critic.forward(states, old_actions.detach()).flatten()
+            critic_value = agent.critic.forward(states, old_actions).flatten()
 
-            target = rewards[:,agent_idx] + agent.gamma*critic_value_
+            target = rewards[:,agent_idx].float() + agent.gamma*critic_value_
+
+
             critic_loss = F.mse_loss(target, critic_value)
             agent.critic.optimizer.zero_grad()
-            critic_loss.backward(retain_graph=True)
+            critic_loss.backward(retain_graph=True, inputs=list(agent.critic.parameters()))
             agent.critic.optimizer.step()
 
-            actor_loss = agent.critic.forward(states, mu[agent_idx]).flatten()
+            actor_loss = agent.critic.forward(states, mu).flatten()
             actor_loss = -T.mean(actor_loss)
             agent.actor.optimizer.zero_grad()
-            actor_loss.backward(retain_graph=True)
+            actor_loss.backward(retain_graph=True, inputs=list(agent.actor.parameters()))
             agent.actor.optimizer.step()
 
             agent.update_network_parameters()
--- FinalProjectCS211/multiagent-particle-envs/MADDPG/main.py
+++ FinalProjectCS211/multiagent-particle-envs/MADDPG/main.py
@@ -18,7 +18,8 @@ file_path = os.path.join(reward_records_dir, file_name) # Path to target folder
 import numpy as np
 from maddpg import MADDPG
 from buffer import MultiAgentReplayBuffer
-from make_env import make_env
+from pettingzoo.mpe import simple_adversary_v3
+import warnings
 import time
 
 def obs_list_to_state_vector(observation):
@@ -27,76 +28,82 @@ def obs_list_to_state_vector(observation):
         state = np.concatenate([state, obs])
     return state
 
+
 if __name__ == '__main__':
-    #scenario = 'simple'
+    # scenario = 'simple'
     scenario = 'simple_adversary'
-    env = make_env(scenario)
-    n_agents = env.n
+    env = simple_adversary_v3.parallel_env(N=2, max_cycles=40, continuous_actions=True)
+    obs = env.reset()
+
+    n_agents = env.num_agents
     actor_dims = []
-    for i in range(n_agents):
-        actor_dims.append(env.observation_space[i].shape[0])
+    for agent_name in env.agents:
+        actor_dims.append(env.observation_spaces[agent_name]._shape[0])
     critic_dims = sum(actor_dims)
 
     # action space is a list of arrays, assume each agent has same action space
-    n_actions = env.action_space[0].n
-    maddpg_agents = MADDPG(actor_dims, critic_dims, n_agents, n_actions, 
-                           fc1=64, fc2=64,  
+    n_actions = 5
+    maddpg_agents = MADDPG(actor_dims, critic_dims, n_agents, n_actions,
+                           fc1=64, fc2=64,
                            alpha=0.01, beta=0.01, scenario=scenario,
                            chkpt_dir='tmp/maddpg/')
 
-    memory = MultiAgentReplayBuffer(1000000, critic_dims, actor_dims, 
-                        n_actions, n_agents, batch_size=1024)
+    memory = MultiAgentReplayBuffer(1000000, critic_dims, actor_dims,
+                                    n_actions, n_agents, batch_size=1024,
+                                    agent_names=env.agents)
 
     PRINT_INTERVAL = 500
     N_GAMES = 25000
-    MAX_STEPS = 25
+    MAX_STEPS =25
     total_steps = 0
     score_history = []
     evaluate = False
-    best_score = 0
-
-    # if evaluate:
-    #     maddpg_agents.load_checkpoint()
-
+    best_score = -3
+    seeds = np.load("/Users/cicilian/Desktop/Multi-Agent-Deep-Deterministic-Policy-Gradients-PyTorch/seed.npy")
+    if evaluate:
+        maddpg_agents.load_checkpoint()
+    time.sleep(1)
     for i in range(N_GAMES):
-        obs = env.reset()
+        obs, _ = env.reset(seed=i)
         score = 0
-        done = [False]*n_agents
+        done = [False] * n_agents
         episode_step = 0
         while not any(done):
-            # if evaluate:
-            #     env.render()
-            #     time.sleep(0.1) # to slow down the action for the video
+            if evaluate:
+                env.render()
+
+                time.sleep(0.12) # to slow down the action for the video
+
             actions = maddpg_agents.choose_action(obs)
-            obs_, reward, done, info = env.step(actions)
-            # print(reward)
-            state = obs_list_to_state_vector(obs)
-            state_ = obs_list_to_state_vector(obs_)
+            obs_, reward, termination, truncation, _ = env.step(actions)
+            state = np.concatenate([i for i in obs.values()])
+            state_ = np.concatenate([i for i in obs_.values()])
 
             if episode_step >= MAX_STEPS:
-                done = [True]*n_agents
+                done = [True] * n_agents
+
+            if any(termination.values()) or any(truncation.values()) or (episode_step >= MAX_STEPS):
+                done = [True] * n_agents
+
             memory.store_transition(obs, state, actions, reward, obs_, state_, done)
-            # print("obs", obs)
-            # print("noise act", actions)
-            # print("ep", i, reward)
-            # print(done)
-            # print("obs_", obs_)
+
             if total_steps % 100 == 0 and not evaluate:
                 maddpg_agents.learn(memory)
-                
+
             obs = obs_
 
-            score += sum(reward)
+            score += sum(reward.values())
             total_steps += 1
             episode_step += 1
 
         score_history.append(score)
         avg_score = np.mean(score_history[-100:])
         if not evaluate:
-            if avg_score > best_score:
-                # maddpg_agents.save_checkpoint()
+
+            if (avg_score > best_score) and (i > PRINT_INTERVAL):
+                print(" avg_score, best_score", avg_score, best_score)
+                maddpg_agents.save_checkpoint()
                 best_score = avg_score
         if i % PRINT_INTERVAL == 0 and i > 0:
             print('episode', i, 'average score {:.1f}'.format(avg_score))
-            print('best score', best_score)
     np.save(file_path, score_history)
