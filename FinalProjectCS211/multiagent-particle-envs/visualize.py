922517edb8b354fe6bb1186d75ca7bfd4bd428ee
huynhchinhan <nhan00554@gmail.com>
2024-12-17 22:16:15 +0700

    add maddpg reward record

--- FinalProjectCS211/multiagent-particle-envs/visualize.py
+++ FinalProjectCS211/multiagent-particle-envs/visualize.py
@@ -19,18 +19,18 @@ def plot(data):
     plt.ylabel("Average Score")
     plt.grid()
     for name, score in data.items():
-        average = moving_average(score, 10)
+        average = moving_average(score, 10000)
         plt.plot(average, label=name)
 
     plt.legend()
     plt.show()
-file = ["maac_reward_record_2.npy", "maddpg_reward_record_1.npy", "madqn_reward_record_1.npy", "ddpg_reward_record.npy"]
+file = ["maac_reward_record_2.npy", "maddpg_reward_record.npy", "madqn_reward_record_1.npy", "ddpg_reward_record.npy"]
 dir = []
 for i in range(len(file)):
     dir.append(os.path.join(reward_records_dir, file[i]))
 data = {
-    "Actor Critic": np.load(dir[0]),
-    # "MADDPG": np.load(dir[1]),
+    # "Actor Critic": np.load(dir[0]),
+    "MADDPG": np.load(dir[1]),
     # "DQN": np.load(dir[2]),
     # "DDPG": np.load(dir[3])
 }
