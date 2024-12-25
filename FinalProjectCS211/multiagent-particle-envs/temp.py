bafca5235ff95e4b11bb2da8df5826c301f0050f
huynhchinhan <nhan00554@gmail.com>
2024-12-25 21:45:29 +0700

    wow

--- FinalProjectCS211/multiagent-particle-envs/temp.py
+++ FinalProjectCS211/multiagent-particle-envs/temp.py
@@ -3,14 +3,17 @@ import os
 import sys
 
 reward_record_path = "/Users/cicilian/Desktop/CS211/FinalProjectCS211/multiagent-particle-envs/reward_records"
-name = "maac_reward_record"
+name = "ddpg_reward_record"
 epochs = "50000"
 files = [os.path.join(reward_record_path, f) for f in os.listdir(reward_record_path) if f.startswith(name+"_"+epochs) and not f.endswith("mean.npy")]
-print(files)
-# print(files[3:4])
-all_arrays = [np.load(file) for file in files]
-for  array in all_arrays:
-    print(array.shape)
-mean = np.mean(all_arrays, axis=0)
 
-np.save(os.path.join(reward_record_path, f"{name}_{epochs}_mean.npy"), mean)
+# print(files)
+
+for i in range(len(files)):
+    array1 = np.load(files[i])
+
+    for j in range(i+1, len(files)):
+        array2 = np.load(files[j])
+
+        if np.array_equal(array1, array2):
+            print(f"Files {files[i]} and {files[j]} are equal\n")
